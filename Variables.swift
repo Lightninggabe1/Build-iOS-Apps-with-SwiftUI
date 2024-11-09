878b4463756572c82058d707adb67e4819450084
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-09 02:59:05 +0000

    HI

--- Variables.swift
+++ Variables.swift
@@ -6,3 +6,17 @@ var year = 2024
 print(year)
 
 //3/9: Arithmetic Operators
+var amount = 18.50
+
+// Write your code below 🧾
+var tip = amount*0.2
+print(tip)
+
+//4/9: Compound Assignment Operators
+var apples = 16
+
+// Write your code below 🍎
+apples -= 4
+apples /= 2
+
+print(apples)
