446a998900ac3055c460377640d21ccc69cc5a91
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-10 04:00:05 +0000

    2/11

--- Conditionals.swift
+++ Conditionals.swift
@@ -1,1 +1,9 @@
 //1/11: Introduction to Conditionals
+
+//2/11: If Statement
+// Write your code below 
+var learningToCode: Bool = true
+learningToCode = false
+if learningToCode {
+  print("Don't forget to take breaks! You got this")
+}
