fca0118f0ea1384f4dc4d49268b5a9b46951764c
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-11 09:43:02 +0000

    4 & 3/11

--- Conditionals.swift
+++ Conditionals.swift
@@ -7,3 +7,25 @@ learningToCode = false
 if learningToCode {
   print("Don't forget to take breaks! You got this")
 }
+
+//3/11: Else Statement
+// Write your code below 👓
+var wearGlasses: Bool = false
+if wearGlasses {
+  print("I wear glasses")
+}
+else {
+  print("I don't wear glasses")
+}
+
+//4/11: Comparison Operators
+let tuneSquadPoints = 78
+let monstarsPoints = 77
+
+// Write your code below 🏀
+if tuneSquadPoints < monstarsPoints {
+  print("Monstars win!")
+}
+else {
+  print("tuneSquad win!")
+}
