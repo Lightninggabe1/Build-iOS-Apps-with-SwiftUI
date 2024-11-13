5950583621964dfc13af069e7bec181dff0502d6
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-13 03:26:18 +0000

    7/11: Switch Statement

--- Conditionals.swift
+++ Conditionals.swift
@@ -65,3 +65,34 @@ if windy {
 
 // Write your code below ⛵
 windy ? print("Sails up") : print("Motor on")
+
+//7/11: Switch Statement
+var secretIdentity = "Tony Stark"
+var superheroName: String 
+ 
+if secretIdentity == "Tony Stark" {
+  superheroName = "Iron Man"
+} else if secretIdentity == "Natasha Romanoff" {
+  superheroName = "Black Widow" 
+} else if secretIdentity == "Prince T'Challa" {
+  superheroName = "Black Panther"
+} else if secretIdentity == "Thor" {
+  superheroName = "Thor"
+} else {
+  superheroName = "Unknown"
+}
+
+// Write your code below
+switch secretIdentity {
+  case "Tony Stark":
+    superheroName = "Iron Man"
+  case "Natasha Romanoff":
+    superheroName = "Black Widow"
+  case "Prince T'Challa":
+    superheroName = "Black Panther"
+  case "Thor":
+    superheroName = "Thor"
+  default:
+    superheroName = "Unknown"
+}
+print(superheroName)
