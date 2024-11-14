749e0d101156319372c3f2f5631a5cc443adfe69
Lightninggabe1 <lightninggabe191@gmail.com>
2024-11-14 03:51:26 +0000

    10/11

--- Conditionals.swift
+++ Conditionals.swift
@@ -127,3 +127,16 @@ switch planet {
   default:
     print("Unknown planet")
 }
+
+//10/11: Switch Statement: Where Clause:
+var wholeNumber = Int.random(in: 10...20)
+print(wholeNumber)
+// Write your code below 
+switch wholeNumber {
+  case let x where x % 2 == 0:
+    print("Composite")
+  case let x where x % 3 == 0:
+    print("Composite")
+  default:
+    print("Prime")
+}
